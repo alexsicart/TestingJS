03d49dc3bce01afab332982fc0626e10f1202008
Alex Sicart Ramos <alexsicart@MacBook-Pro-de-Alex.local>
2017-03-27 18:23:09 +0200

    Add Time Converter

---
+++ TimeConverter.js
@@ -0,0 +1,11 @@
+function TimeConvert(num) {
+
+  // code goes here
+  var hours = Math.floor(num/60);
+  var minutes = num % 60;
+  return hours + ':' + minutes;
+
+}
+
+// keep this function call here
+TimeConvert(readline());  
